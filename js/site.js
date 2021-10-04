12b53114893b2aace3901438b8ad4f5896267462
WILLIAMTHEWEST1 <90918613+WILLIAMTHEWEST1@users.noreply.github.com>
2021-10-04 16:09:40 -0500

    got it so far

---
+++ js/site.js
@@ -0,0 +1,18 @@
+//displays a message to the user
+function displayMessage() {
+
+    let msg = "";
+
+    msg = document.getElementById("message").value;
+
+    alert(msg);
+}
+
+function displayMessage2() {
+
+let msg = "";
+
+msg = document.getElementById("message").value;
+
+Swal.fire(msg)
+}
