95c50210b3cfc8fec7f0e3c73e6156ddca62049a
WILLIAMTHEWEST1 <90918613+WILLIAMTHEWEST1@users.noreply.github.com>
2021-10-08 16:09:30 -0500

    initial commit

--- js/site.js
+++ js/site.js
@@ -1,18 +1,51 @@
-//displays a message to the user
-function displayMessage() {
+//get starting values from the screen
+//Controller Function
+function getValues() {
+      //Mortgage Calculator
+    //Step 1 - Get User inputs - this will access the DOM and call all other functions
+    function getValues() {
+        let loanAmount = document.getElementById("someId").value;
 
-    let msg = "";
+        let term = document.getElementById("anotherId").value;
 
-    msg = document.getElementById("message").value;
+        let interestRate = document.getElementById("thirdId").value;
 
-    alert(msg);
-}
+        let calculatedRate = calcRate(interestRate);
 
-function displayMessage2() {
+        let payment = calcPayment(loanAmount, calculatedRate, term);
 
-let msg = "";
+        let totalPayments = calcPaymentSchedule(loanAmount, calculatedRate, term, payment);
 
-msg = document.getElementById("message").value;
+        displayData(totalPayments);
 
-Swal.fire(msg)
-}
+    }
+
+    function getValues2() {
+        let msg = "";
+        //Step 2 - Calculate the interest rate - this is the division by 1200 from the PDF
+        function calcRate() {}
+
+        //Step 3 - Calculate monthly payment - Amount times rate divided by the complicated math from the PDF Math.pow()
+        function calcPayment() {}
+
+        //Step 3.5 - Calculate the interest rate based on the current balance in rate from step 2
+        //This function is called in Step 4, but you need to build it first
+        //Calculates interest per payment
+        function calcInterest() {}
+
+        msg = document.getElementById("message").value;
+        //Step 4 - Now that we know the rate and monthly payment we build out our payment schedule
+        //This is the big function of the application - create your object in this function
+        //let obj = {
+        //     month:
+        //     payment:
+        //     principal:
+        //     interest:
+        //     totalInterest:
+        //     balance:
+        // }
+        function calcPaymentSchedule() {}
+    }
+    //Step 5 - Display all of the calculated information
+    //Use the template structure from FizzBuzz to output your data
+    function displayData() {}
